e0437c2f241c7e17697959f6449cf859e4f0ab0f
Reza-Rahmani1378 <reza6273757@gmail.com>
2023-10-09 18:59:08 +0330

    Create Publisher

--- demo/src/test/java/com/java/webflux/demo/SpringbootWebfluxDemoApplicationTests.java
+++ demo/src/test/java/com/java/webflux/demo/SpringbootWebfluxDemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.java.webflux.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class SpringbootWebfluxDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testMono() {
+		Mono<String> monoString = Mono.just("Java").log();
+		monoString.subscribe(System.out::println);
 	}
 
 }
