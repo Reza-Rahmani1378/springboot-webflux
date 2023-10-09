0e16070d83ceb160143c3365184faac5a6f861b7
Reza-Rahmani1378 <reza6273757@gmail.com>
2023-10-09 18:31:15 +0330

    Init Spring-Boot Web-flux Project

---
+++ demo/src/test/java/com/java/webflux/demo/SpringbootWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.webflux.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
